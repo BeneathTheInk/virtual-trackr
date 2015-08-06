44ba25600e1910ef541a1a2ecaaaa844efface59
Tyler Johnson <tyler@vintyge.com>
2015-08-06 10:29:27 -0600

    new start

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+/*
+
+Virtual tracker is designed to
+- generate virtual dom nodes
+- within an autorun context
+- as a virtual dom widget.
+
+*/
+
 var Trackr;
 var VText = require("virtual-dom/vnode/vtext");
 var createElement = require('virtual-dom/create-element');
-var diff = require('virtual-dom/diff');
-var patch = require('virtual-dom/patch');
+var diff = require('./lib/multidiff.js');
+var patch = require('./lib/multipatch.js');
 var matchesSelector = require("matches-selector");
+var _ = require("underscore");
+var utils = require("./lib/utils.js");
 
 try {
 	Trackr = require("trackr");
@@ -21,96 +32,180 @@ module.exports = VTrackr;
 
 VTrackr.prototype.type = "Widget";
 
-VTrackr.prototype.autorun = function(fn) {
-	return Trackr.autorun(fn, this);
+// returns an array with the parent and nextSibling nodes of this widget
+VTrackr.prototype.position = function() {
+	var before, parent;
+	var lastNode = _.last(this.nodes);
+
+	if (lastNode) {
+		before = lastNode.nextSibling;
+		parent = lastNode.parentNode;
+	} else {
+		before = this.placeholder;
+	}
+
+	if (before && !parent) parent = before.parentNode;
+
+	return before && parent ? new utils.Position(parent, before) : null;
 };
 
-VTrackr.prototype.mount = function() {
-	this.stop();
-
-	this.comp = this.autorun(function(comp) {
-		var newtree = this.render(comp);
-
-		if (typeof newtree === "string") {
-			newtree = new VText(newtree);
-		}
-
-		this._latestTree = newtree;
-		if (this.node) this.refresh();
-
-		comp.onInvalidate(function() {
-			if (comp.stopped) {
-				this._latestTree = null;
-				delete this.comp;
-			}
-		});
-	});
-
-	return this;
-};
-
-VTrackr.prototype.stop = function() {
-	if (this.comp) this.comp.stop();
-	return this;
-};
-
-VTrackr.prototype.refresh = function() {
+VTrackr.prototype.draw = function() {
 	// must have changed
 	if (!this._latestTree) return this;
 
-	// create a new node if none is set
-	if (!this.node || !this._tree) {
-		this.node = createElement(this._latestTree);
+	// get dom position
+	var pos = this.position();
+
+	// if not in the DOM, take everything down
+	if (!pos) return this.clearNodes();
+
+	// create new nodes if none is set
+	if (!this.nodes || !this._tree) {
+		this.nodes = this._latestTree.map(function(t) {
+			return createElement(t);
+		});
 	}
 
 	// otherwise patch and apply new tree
 	else {
-		this.node = patch(this.node, diff(this._tree, this._latestTree));
+		this.nodes = multivdom.patch(
+			pos, this.nodes.slice(0),
+			multivdom.diff(this._tree, this._latestTree)
+		);
 	}
 
+	// apply all nodes onto the dom
+	utils.insertNodes(this.nodes, pos);
+
+	// update internal tree
 	this._tree = this._latestTree;
 	this._latestTree = null;
 
 	return this;
-};
+},
 
-VTrackr.prototype.init = function() {
-	this.refresh();
-	return this.node;
-};
-
-VTrackr.prototype.update = function(previous, node) {
-	var prevtree = previous && previous._tree;
-	if (previous) previous.destroy();
-	if (!prevtree || !node) return this.init();
-
-	// basically, don't be switching trees without calling destroy
-	if (this.node && this.node !== node) {
-		throw new Error("This VTrackr was already initated with a different node.");
-	}
-
-	// update internal state and update the node
-	this.node = node;
-	if (!this._latestTree) this._latestTree = this._tree;
-	this._tree = prevtree;
-	this.refresh();
-
-	return this.node;
-};
-
-VTrackr.prototype.destroy = function(node) {
-	this.stop();
-	this.node = null;
-	this._tree = null;
-};
-
-VTrackr.prototype.paint = function(parent, before) {
-	if (typeof parent === "string") parent = document.querySelector(parent);
-	if (parent == null) throw new Error("Expecting a valid DOM element to attach in.");
-	if (typeof before === "string") before = parent.querySelector(before);
-	parent.insertBefore(this.init(), before);
+VTrackr.prototype.updateTree = function(newtree) {
+	this._latestTree = newtree;
+	this.draw();
 	return this;
 };
+
+VTrackr.prototype.clear = function() {
+	if (this.nodes) this.nodes.forEach(function(n) {
+		if (n.parentNode) n.parentNode.removeChild(n);
+	});
+
+	this.nodes = null;
+	this._tree = null;
+
+	return this;
+};
+
+// called by vdom when being freshly generated
+VTrackr.prototype.init = function() {
+
+};
+
+// called by vdom when new, but replacing something existing
+VTrackr.prototype.update = function() {
+
+};
+
+// called when removed from the dom or needs to be otherwise killed
+VTrackr.prototype.destroy = function() {
+
+};
+
+// VTrackr.prototype.autorun = function(fn) {
+// 	return Trackr.autorun(fn, this);
+// };
+//
+// VTrackr.prototype.mount = function() {
+// 	this.stop();
+//
+// 	this.comp = this.autorun(function(comp) {
+// 		var newtree = this.render(comp);
+//
+// 		if (typeof newtree === "string") {
+// 			newtree = new VText(newtree);
+// 		}
+//
+// 		this._latestTree = newtree;
+// 		if (this.node) this.refresh();
+//
+// 		comp.onInvalidate(function() {
+// 			if (comp.stopped) {
+// 				this._latestTree = null;
+// 				delete this.comp;
+// 			}
+// 		});
+// 	});
+//
+// 	return this;
+// };
+//
+// VTrackr.prototype.stop = function() {
+// 	if (this.comp) this.comp.stop();
+// 	return this;
+// };
+//
+// VTrackr.prototype.refresh = function() {
+// 	// must have changed
+// 	if (!this._latestTree) return this;
+//
+// 	// create a new node if none is set
+// 	if (!this.node || !this._tree) {
+// 		this.node = createElement(this._latestTree);
+// 	}
+//
+// 	// otherwise patch and apply new tree
+// 	else {
+// 		this.node = patch(this.node, diff(this._tree, this._latestTree));
+// 	}
+//
+// 	this._tree = this._latestTree;
+// 	this._latestTree = null;
+//
+// 	return this;
+// };
+//
+// VTrackr.prototype.init = function() {
+// 	this.refresh();
+// 	return this.node;
+// };
+//
+// VTrackr.prototype.update = function(previous, node) {
+// 	var prevtree = previous && previous._tree;
+// 	if (previous) previous.destroy();
+// 	if (!prevtree || !node) return this.init();
+//
+// 	// basically, don't be switching trees without calling destroy
+// 	if (this.node && this.node !== node) {
+// 		throw new Error("This VTrackr was already initated with a different node.");
+// 	}
+//
+// 	// update internal state and update the node
+// 	this.node = node;
+// 	if (!this._latestTree) this._latestTree = this._tree;
+// 	this._tree = prevtree;
+// 	this.refresh();
+//
+// 	return this.node;
+// };
+//
+// VTrackr.prototype.destroy = function(node) {
+// 	this.stop();
+// 	this.node = null;
+// 	this._tree = null;
+// };
+//
+// VTrackr.prototype.paint = function(parent, before) {
+// 	if (typeof parent === "string") parent = document.querySelector(parent);
+// 	if (parent == null) throw new Error("Expecting a valid DOM element to attach in.");
+// 	if (typeof before === "string") before = parent.querySelector(before);
+// 	parent.insertBefore(this.init(), before);
+// 	return this;
+// };
 
 VTrackr.prototype.findAll = function(selector) {
 	var matches = [],
